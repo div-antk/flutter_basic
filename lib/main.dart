732d81c55c1b6416695577e299c8fd58821d6a51
div-antk <51283468+owaii@users.noreply.github.com>
2023-05-13 23:59:00 +0900

    Todoの雛形を作成

--- lib/main.dart
+++ lib/main.dart
@@ -71,6 +71,34 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Text('リスト一覧画面'),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// リスト追加画面用
+class TodoAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            // "pop" で前の画面に戻る
+            Navigator.of(context).pop();
+          },
+          child: Text('リスト追加画面（クリックで戻る）'),
+        ),
+      ),
     );
   }
 }
